9d9a63367c58883de6f2c02427b99cd7844b96fe
Ruan Heng <ruanheng1995@gmail.com>
2021-12-24 22:58:06 +0800

    添加项目文件。

---
+++ Controllers/TopNavigationController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blog_admin_api.Controllers
+{
+    public class TopNavigationController : Controller
+    {
+    }
+}
---
+++ Models/CommonResponse.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blog_admin_api.Models
+{
+    public class CommonResponse
+    {
+        public bool Successed { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+        public int StatusCode { get; set; } = 200;
+    }
+}
